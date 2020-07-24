c25d0e7bdc132070d608e2cdb9336a9e5ad145ee
Tong Yan <gnotnay@gmail.com>
2020-07-24 00:38:04 -0700

    add lc763 (Mike) (Two pointers)

--- README.md
+++ README.md
@@ -14,3 +14,4 @@
 ## Week1  
 [103. Binary Tree Zigzag Level Order Traversal (M)](https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal)  
 [1104. Path In Zigzag Labelled Binary Tree (M)](https://leetcode.com/problems/path-in-zigzag-labelled-binary-tree/)  
+[763. Partition Labels (M)](https://leetcode.com/problems/partition-labels/)  
---
+++ week1/lc763_partitionLabels.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} S
+ * @return {number[]}
+ */
+var partitionLabels = function(s) {
+    let ret = [];
+    let left = 0, right = 0;
+    let tempLength = 1;
+    let set = new Set();
+    
+    let last = new Array(26).fill(0);
+    
+    for (let i = 0; i < s.length; ++i)
+        last[s.charCodeAt(i) - 'a'.charCodeAt(0)] = i;
+    
+    for (let start = 0; start < s.length; start++) {
+        let end = 0;
+        if (!set.has(s[start])) {
+            end = last[s.charCodeAt(start) - 'a'.charCodeAt(0)];
+            // if we find same char => handle "right" => {1.end <= right (no change), 2. end > right (update right)}
+            if (end > right) {
+                right = end;
+                tempLength = right - left + 1;
+            }
+        }
+        
+        set.add(s[start]);
+        
+        if (start === right) {
+            ret.push(tempLength);
+            right = 0;
+            left = start + 1;
+            tempLength = 1;
+        }
+    }
+    
+    return ret;
+};
