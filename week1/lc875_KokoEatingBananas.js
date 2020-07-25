098925cc4b6c53ff4b9404a25f524669d6eddb3d
Tong Yan <gnotnay@gmail.com>
2020-07-25 10:36:14 -0700

    update lc875

--- week1/lc875_KokoEatingBananas.js
+++ week1/lc875_KokoEatingBananas.js
@@ -5,42 +5,22 @@
  */
 var minEatingSpeed = function(piles, H) {
     let minSpeed = 1;
-    let maxSpeed = piles[0];
-    
-    for (let i = 1; i < piles.length; i++) {
-        if (piles[i] > maxSpeed) {
-            maxSpeed = piles[i];
-        }
-    }
+    let maxSpeed = Math.max(...piles);
     
     while (minSpeed + 1 < maxSpeed) {
         let mid = minSpeed + Math.floor((maxSpeed - minSpeed) / 2);
-        if (finishInH(piles, mid) > H) {
-            minSpeed = mid;
-        } else {
-            maxSpeed = mid;
-        }
+        finishInH(piles, mid) > H ? minSpeed = mid : maxSpeed = mid;
     }
     
-    if (finishInH(piles, minSpeed) <= H) {
-        return minSpeed;
-    }
-    
-    if (finishInH(piles, maxSpeed) <= H) {
-        return maxSpeed;
-    }
+    return finishInH(piles, minSpeed) <= H ? minSpeed : maxSpeed;
 };
 
 let finishInH = (piles, speed) => {
     let hour = 0;
     
-    for (let i = 0; i < piles.length; i++) {
-        if (piles[i] % speed === 0) {
-            hour += Math.floor(piles[i] / speed);
-        } else {
-            hour += (Math.floor(piles[i] / speed) + 1);
-        }
-    }
+    piles.forEach(p => {
+        hour += Math.ceil(p / speed);
+    });
     
     return hour;
 }
