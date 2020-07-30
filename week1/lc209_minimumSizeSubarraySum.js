f4b7feaeb64ea8f57c134f5672a0b418d210c1dd
Houjue92 <hwang7@wpi.edu>
2020-07-30 04:01:46 -0700

    add nlogn solution

--- week1/lc209_minimumSizeSubarraySum.js
+++ week1/lc209_minimumSizeSubarraySum.js
@@ -27,3 +27,51 @@ var minSubArrayLen = function(s, nums) {
     
     return (min < nums.length + 1)? min: 0;
 }
+
+
+//follow up, nlogn solution
+/**
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(s, nums) {
+    if(nums === null || nums.length === 0) return 0;
+    
+    let min = nums.length + 1;
+    let sums = new Array(nums.length);
+    
+    sums[-1] = 0;
+    for(let i = 0; i < nums.length; i++) { 
+       sums[i] = sums[i - 1] + nums[i];
+    }
+    
+    for(let left = 0; left < nums.length; left++) {
+        let right = bfs(left, nums, sums, s);
+        if(right !== -1) min = Math.min(right - left + 1, min);
+    }
+    
+    return (min < nums.length + 1)? min:0;
+}
+
+const bfs = (left, nums, sums ,s) => {
+    let start = left, end = nums.length - 1;
+    while(start + 1 < end) {
+        let mid = start + Math.floor((end - start)/2);
+        let value = sums[mid] - sums[left - 1]
+        if( value>= s) {
+            end = mid;
+        } else {
+            start = mid ;
+        }
+    }
+    
+    if(sums[start] - sums[left - 1] >= s) {
+        return start;
+    } else if(sums[end] - sums[left - 1] >= s) {
+        return end;
+    } else {
+        return -1;
+    }
+}
+
